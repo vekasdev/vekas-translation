21ae5d470990305166c49ee78d8eaaaccf428d2d
ahmedJS <ahmed_alsadi56@outlook.com>
2024-07-27 23:19:11 +0300

    re write and complete the tests that is about supporting the pairs of two languages automaticly

--- tests/JsonFIleLangHandlerTest.php
+++ tests/JsonFIleLangHandlerTest.php
@@ -21,4 +21,103 @@ class JsonFIleLangHandlerTest extends TestCase {
         
         $this->jsonFileLangHandler->addItem("bee","نحلة");
     }
+
+
+    function testFindMappingFiles() {
+        $mappings = $this->jsonFileLangHandler->findMappings();
+        $this->assertSame($mappings,[
+            [
+                "sourceLanguage" => "en",
+                "targetLanguage" => "ar"
+            ]
+        ]);
+    }
+
+
+
+    function testGetMappingInfo(){ 
+        $result = $this->jsonFileLangHandler->getMappingInfo("en2ar.json");
+
+        $this->assertSame([
+            "sourceLanguage" => "en",
+            "targetLanguage" => "ar"
+        ],$result);
+    }
+
+    function testGetFileNameByPath() {
+        $result = $this-> jsonFileLangHandler->getFileName("myage/mainfolder/index.php");
+        $this->assertSame("index.php",$result);
+    }
+
+
+    function testLanguageNotSwappedByDefault() {
+        $this->assertNotTrue( $this->jsonFileLangHandler->isSwapped());
+        $this->assertSame("en",$this->jsonFileLangHandler->getSource());
+        $this->assertSame("ar",$this->jsonFileLangHandler->getTarget());
+    }
+
+    function testSwapingLanguage() {
+        $this->jsonFileLangHandler->swapLanguages();
+        $this->jsonFileLangHandler->swapState();
+        
+        $this->assertTrue( $this->jsonFileLangHandler->isSwapped());
+        $this->assertSame("ar",$this->jsonFileLangHandler->getSource());
+        $this->assertSame("en",$this->jsonFileLangHandler->getTarget());
+    }
+
+    function testSwappingTwice() {
+        $this->jsonFileLangHandler->swapLanguages();
+        $this->jsonFileLangHandler->swapLanguages();
+        $this->assertNotTrue( $this->jsonFileLangHandler->isSwapped());
+        $this->assertSame("en",$this->jsonFileLangHandler->getSource());
+        $this->assertSame("ar",$this->jsonFileLangHandler->getTarget());
+    }
+
+    function testGetPathWhenSwapping() {
+        // swap one
+        $this->jsonFileLangHandler->swapLanguages();
+        $fileName = basename($this->jsonFileLangHandler->getFullPath());
+        $this->assertSame("ar2en.json",$fileName);
+
+        // swap two
+        $this->jsonFileLangHandler->swapLanguages();
+        $fileName = basename($this->jsonFileLangHandler->getFullPath());
+        $this->assertSame("en2ar.json",$fileName);
+    }
+
+    
+    function testLoadSwappedVersionOfJsonFileHandler() {
+
+        $fileHandler = $this->loadSwappedVersionOfJsonFileHandler();
+
+        $this->assertInstanceOf(JsonFileLangHandler::class,$fileHandler);
+        $this->assertTrue($fileHandler->isSwapped());
+    }
+
+
+
+    function testAddEntryInSwappedMode() {
+        $handler = $this->loadSwappedVersionOfJsonFileHandler();
+        $itemToAdd = "شيء";
+        
+        try {
+            $handler->addItem($itemToAdd,"thing");
+        } catch (ItemAlreadyExistException $e ) {
+
+        }
+
+        $this->assertTrue(isset($handler->getData()["شيء"]));
+        $this->assertSame("thing",$handler->getData()["شيء"]);
+    }
+
+
+
+
+    function loadSwappedVersionOfJsonFileHandler() {
+        $handler =  new JsonFileLangHandler(__DIR__."/dics","2","ar","en");
+        $handler->load();
+        return $handler;
+    }
+
+
 }
